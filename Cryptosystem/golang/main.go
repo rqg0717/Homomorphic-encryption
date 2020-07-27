95cb849d8253ce391b718f73c61a53a47d5be801
James Ren <rqg0717@gmail.com>
2020-07-26 21:00:27 -0500

    debugging

--- Cryptosystem/golang/main.go
+++ Cryptosystem/golang/main.go
@@ -44,9 +44,9 @@ func genCoPrime(this *big.Int) *big.Int {
 	var result *big.Int
 	for done == 0 {
 		result, _ = rand.Prime(rand.Reader, bitLength)
-
 		// gcd test
-		gcd := result.GCD(nil, nil, result, this)
+		gcd := big.NewInt(0)
+		gcd.GCD(nil, nil, result, this)
 		if gcd.Int64() == int64(1) {
 			done = 1
 		}
@@ -56,25 +56,42 @@ func genCoPrime(this *big.Int) *big.Int {
 
 // Encryption Encrypts plaintext m. Ciphertext em = g^m * r^n mod n^2.
 func Encryption(m *big.Int) big.Int {
-	var em big.Int
+	var em, gm, rn big.Int
 	//generates random input r
 	r := genCoPrime(m)
-	gm := g.Exp(&g, m, nil)
-	rn := r.Exp(r, &n, nil)
-	em.Mul(gm, rn)
+	fmt.Println("g: ", g)
+	fmt.Println("n: ", n)
+	gm.Exp(&g, m, nil)
+	rn.Exp(r, &n, nil)
+	em.Mul(&gm, &rn)
 	em.Mod(&em, &nsqr)
 	return em
 }
 
 // Decryption Decrypts ciphertext em. Plaintext m = L(c^lambda mod n^2) * u mod n
-func Decryption(em *big.Int) *big.Int {
-	var m *big.Int
-
+// where u = (L(g^lambda mod n^2))^(-1) mod n.
+func Decryption(em *big.Int) big.Int {
+	var m, u big.Int
+	fmt.Println("g: ", g)
+	fmt.Println("lambda: ", lambda)
+	u.Exp(&g, &lambda, nil)
+	u.Sub(&u, big.NewInt(1))
+	u.Div(&u, &n)
+	u.ModInverse(&u, &n)
+	m.Exp(em, &lambda, nil)
+	m.Sub(&m, big.NewInt(1))
+	m.Div(&m, &n)
+	m.Mul(&m, &u)
+	m.Mod(&m, &n)
 	return m
 }
 
 func main() {
 	KeyGeneration()
 	m := big.NewInt(11)
-	fmt.Println("Results: ", Encryption(m))
+	em := Encryption(m)
+	dm := Decryption(&em)
+	fmt.Println("Encryption: ", em)
+	fmt.Println("Decryption: ", dm)
+
 }
